d3fb9325189215c6c77bca775865a3af423fdc74
nczitzk <ethansme@outlook.com>
2018-08-15 20:42:36 +0800

    Update login handler

--- handler/login.go
+++ handler/login.go
@@ -33,5 +33,37 @@ func LoginHandler(w http.ResponseWriter, request *http.Request) {
 	}
 	client.Jar = jar
 
+	// GET CAS Login Page
+
+	response, err := client.Get(LOGIN)
+	if err != nil {
+		panic(err)
+	}
+	defer response.Body.Close()
+	body, _ := ioutil.ReadAll(response.Body)
+
+	// Take the value of 'lt' and 'execution' from the CAS Login Page source
+
+	var (
+		lt        string
+		execution string
+	)
+
+	Waitgroup.Add(1)
+	go Reg(&lt, "lt\" value=\"(.*)\"", string(body))
+	Waitgroup.Add(1)
+	go Reg(&execution, "execution\" value=\"(.*)\"", string(body))
+
+	// Take values in the form from request
+
+	request.ParseForm()
+	username := request.FormValue("账号")
+	password := request.FormValue("密码")
+
+	if username == "" && password == "" {
+		fmt.Fprintf(w, "缺少账号或密码")
+		return
+	}
+	
 	return
 }
