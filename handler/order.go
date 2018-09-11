e9315547a3e2f3aecf1988be84c02ad2c9b12a2d
nczitzk <ethansme@outlook.com>
2018-09-11 21:16:40 +0800

    Update order handler

--- handler/order.go
+++ handler/order.go
@@ -17,5 +17,15 @@ func OrderHandler(w http.ResponseWriter, request *http.Request) {
 	w.Header().Add("Access-Control-Allow-Headers", "Content-Type")
 	w.Header().Set("content-type", "application/json")
 
+	// Take value of 'date' from the url
+
+	var date string
+	form, err := url.ParseQuery(request.URL.RawQuery)
+	if err == nil && len(form["date"]) > 0 {
+		date = form["date"][0]
+	}
+
+	var client http.Client
+
 	return
 }
