51f3a5b527cc242b54e8fb9afd9adf8bfcb8577e
nczitzk <ethansme@outlook.com>
2018-09-11 19:49:18 +0800

    Show running status

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"runtime"
 	"net/http"
 	. "handler"
@@ -12,5 +13,9 @@ func main() {
 	http.HandleFunc("/dates/", DatesHandler)
 	http.HandleFunc("/menu/",  MenuHandler)
 	http.HandleFunc("/order/", OrderHandler)
+	http.ListenAndServe(":2018", nil)
+
+	fmt.Println(time.Now().Format("2006/01/02 15:04:05"), "szsy-canteen-api 开始运行")
+
 	http.ListenAndServe(":2018", nil)
 }
