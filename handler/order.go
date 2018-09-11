8ca3911c4223afdf3c3cc9da630b6d4099f3af70
nczitzk <ethansme@outlook.com>
2018-09-11 21:21:06 +0800

    Update order handler

--- handler/order.go
+++ handler/order.go
@@ -52,5 +52,16 @@ func OrderHandler(w http.ResponseWriter, request *http.Request) {
 		Value: SessionId,
 	}
 
+	request, err = http.NewRequest("GET", MENU+date, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	// Add Cookies
+
+	request.AddCookie(&cookie_CasModule)
+	request.AddCookie(&cookie_LcSoftCardV2)
+	request.AddCookie(&cookie_SessionId)
+
 	return
 }
